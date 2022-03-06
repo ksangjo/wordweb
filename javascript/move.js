3039437b304da66bfb4a9e580f6e113a0c78b051
ksangjo <83195444+ksangjo@users.noreply.github.com>
2022-03-06 17:32:46 +0900

    Jsword (#1)
    * iterating word
    
    * add left
    
    Co-authored-by: suhwan kang <lionwithnomane@gmail.com>

--- javascript/move.js
+++ javascript/move.js
@@ -1,9 +1,18 @@
 let count = 0;
 
 function left_click() {
-    count++;
     console.log(count);
-    document.getElementsByClassName('display')[0].innerHTML = count;
+    fetch(`http://127.0.0.1:8000/memorization/total/${count}`, {
+        method:'GET',
+        headers:{
+            'Content-Type': 'application/text'
+        }})
+    .then((response) => response.text().then(function (text) {
+            document.getElementsByClassName('english')[0].innerHTML = JSON.parse(text).word;
+            document.getElementsByClassName('meaning')[0].innerHTML = JSON.parse(text).meaning;
+            document.getElementsByClassName('sentence')[0].innerHTML = JSON.parse(text).sentence;
+            count--;
+        }));
 }
 
 function right_click() {
@@ -14,7 +23,10 @@ function right_click() {
             'Content-Type': 'application/text'
         }})
     .then((response) => response.text().then(function (text) {
-            document.getElementsByClassName('display')[0].innerHTML = text;
+            document.getElementsByClassName('english')[0].innerHTML = JSON.parse(text).word;
+            document.getElementsByClassName('meaning')[0].innerHTML = JSON.parse(text).meaning;
+            document.getElementsByClassName('sentence')[0].innerHTML = JSON.parse(text).sentence;
+            count++;
         }));
 }
 
